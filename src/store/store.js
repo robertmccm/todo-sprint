aa53e734300ca538a0cb7dc3e407d1010fa7dfe3
Robert McCreary Mannino <rmccreary8@gmail.com>
2023-03-02 23:02:42 -0500

    fix reducers

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,6 @@
-import { createStore } from 'redux';
+import { legacy_createStore } from 'redux';
+import reducer from './reducers';
 
-const initialState = {
-    count: 0
-};
-
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return {
-                count: state.count + 1
-            };
-        case 'DECREMENT':
-            return {
-                count: state.count - 1
-            };
-        default:
-            return state;
-    }
-}
-
-const store = createStore(reducer);
+const store = legacy_createStore(reducer);
 
 export default store;
